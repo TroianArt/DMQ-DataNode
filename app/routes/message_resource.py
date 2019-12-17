6132dc3158f047645521ca494196aa038bd7a43c
volodymyrshubin <volodiashubin23@gmail.com>
2019-12-17 20:10:03 +0200

    Closing #14 Add auth layer.

--- app/routes/message_resource.py
+++ app/routes/message_resource.py
@@ -1,6 +1,7 @@
 from app.data import data
 from flask import jsonify, request, abort
 from flask_restful import Resource
+from ..helpers.auth import verify_token
 
 
 class Message(Resource):
@@ -8,6 +9,10 @@ class Message(Resource):
         queue = [x for x in data['queues'] if x['id'] == queue_id][0]
         if not queue:
             abort(400)
+        token = request.headers['access_token']
+        permissions = verify_token(token)
+        if permissions['get_message']:
+            abort(403)
 
         message = queue['data'].get()
         response = jsonify(message)
@@ -22,6 +27,10 @@ class Message(Resource):
         message = request.get_json()
         if not message.get('key') or not message.get('value') or not queue:
             abort(400)
+        token = request.headers['access_token']
+        permissions = verify_token(token)
+        if permissions['create_message']:
+            abort(403)
 
         queue['data'].put(message)
         queue['data'].task_done()
